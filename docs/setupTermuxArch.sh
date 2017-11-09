a07b4950a06dbedc9be562d2ec265bfaafee9fb2
S D Rausty <sdrausty@gmail.com>
2017-11-08 22:17:39 -0500

    Commit on 20171108 at 22:17:39 and at 1510197459 in Unix time.

--- docs/setupTermuxArch.sh
+++ docs/setupTermuxArch.sh
@@ -5,6 +5,8 @@
 # Change mirror to your desired geographic location in `knownconfigurations.sh`.
 ################################################################################
 
+export LD_PRELOAD=$PREFIX/lib/libtermux-exec.so
+
 printf '\033]2;  Thank you for using `setupTermuxArch.sh` 📲 \007'"\n\033[10m 🕛<🕛 This setup script will attempt to set Arch Linux up in your Termux environment.  When successfully completed, you will be enjoying the bash prompt in Arch Linux in Termux on your smartphone or tablet.  \033[32;1mUpdating Termux and installing the required components for Arch Linux installation. This will take some time.  \033[10mIf you do not see 🕐 one o'clock, run this script again. You might want to check your Internet connection.  \n\n"
 
 apt-get -qq update && apt-get -qq upgrade --yes
@@ -12,16 +14,16 @@ apt-get -qq install bsdtar proot termux-exec wget --yes
 
 depend ()
 {
+wget -q -N --show-progress https://raw.githubusercontent.com/sdrausty/TermuxArch/master/archsystemconfigs.sh
 wget -q -N --show-progress https://raw.githubusercontent.com/sdrausty/TermuxArch/master/knownconfigurations.sh
 wget -q -N --show-progress https://raw.githubusercontent.com/sdrausty/TermuxArch/master/necessaryfunctions.sh
 wget -q -N --show-progress https://raw.githubusercontent.com/sdrausty/TermuxArch/master/printoutstatements.sh
 wget -q -N --show-progress https://raw.githubusercontent.com/sdrausty/TermuxArch/master/setupTermuxArch.sh
-wget -q -N --show-progress https://raw.githubusercontent.com/sdrausty/TermuxArch/master/systemconfigs.sh
 wget -q -N --show-progress https://raw.githubusercontent.com/sdrausty/TermuxArch/master/termuxarchchecksum.md5
+. ./archsystemconfigs.sh
 . ./knownconfigurations.sh
 . ./necessaryfunctions.sh
 . ./printoutstatements.sh
-. ./systemconfigs.sh
 }
 
 # Main Block
